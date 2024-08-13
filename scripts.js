256b160ddaf33ae0b7b76e05b67ce50d73055770
MaBoito <matheus_boito@hotmail.com>
2024-08-13 12:38:11 -0300

    new loop

--- scripts.js
+++ scripts.js
@@ -1,9 +1,17 @@
 exibirAsterisco(10);
 
 function exibirAsterisco(linhas) {
-    let padrao = "";
+//     let padrao = "";
+//     for (let linha = 1; linha <= linhas; linha++) {
+//         padrao += "*";
+//             console.log (padrao);
+//     }
+
     for (let linha = 1; linha <= linhas; linha++) {
-        padrao += "*";
-            console.log (padrao);
-    }
+        let padrao = "";
+        for(let i= 0; i < linha; i++){
+            padrao += "*";
+        }
+        console.log (padrao);
+}
 }
