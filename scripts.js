05480c6efb8f6aaa007d8b4e4fa34e1bf675ca73
MaBoito <matheus_boito@hotmail.com>
2024-08-13 12:29:02 -0300

    initial commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+exibirAsterisco(10);
+
+function exibirAsterisco(linhas) {
+    let padrao = "";
+    for (let linha = 1; linha <= linhas; linha++) {
+        padrao += "*";
+            console.log (padrao);
+    }
+}
